5551bba4cef449846bee2aa6ebbea63eb016ccef
Manmohan7 <er.manmohansingh7@gmail.com>
2021-02-27 02:02:12 +0530

    navigation bar update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,36 @@
 import React from 'react'
-import { AppBar, Button, Grid, Toolbar } from '@material-ui/core'
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
+import { AccountCircle } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   toolbar: {
     justifyContent: 'space-between'
   }
-}));
+});
 
 function NavBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   const classes = useStyles();
+  const open = Boolean(anchorEl)
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <AppBar position='static'>
@@ -22,10 +42,35 @@ function NavBar() {
             </Button>
           </Grid>
 
-          <Grid item >
-            <Button component={Link} to='/login' color='inherit'>
-              Login
-            </Button>
+          <Grid item>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem component={Link} to='/login'>Login</MenuItem>
+              <MenuItem component={Link} to='/signup'>Sign up</MenuItem>
+            </Menu>
           </Grid>
         </Grid>
       </Toolbar>
