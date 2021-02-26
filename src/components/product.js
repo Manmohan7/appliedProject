1d39f09003c4a21fce532c1cceb7597177d68a83
Manmohan7 <er.manmohansingh7@gmail.com>
2021-02-27 01:09:52 +0530

    product template

--- src/components/product.js
+++ src/components/product.js
@@ -1,14 +1,46 @@
 import React from 'react'
-import { Card, CardActions, CardContent } from '@material-ui/core'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
-export default function Product() {
+const useStyles = makeStyles({
+  cardContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  }
+})
+
+export default function Product({ product }) {
+  const classes = useStyles();
+
   return (
     <Card>
+      <CardMedia
+        component='img'
+        image='https://images-na.ssl-images-amazon.com/images/I/511ptk4SNrL._UL1001_.jpg'
+        alt='sunglasses'
+        height='200'
+      />
       <CardContent>
-        Hello hello
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            Fake Rayban
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            $30
+          </Typography>
+        </div>
       </CardContent>
       <CardActions>
-        Blah
+        <Button variant="outlined">
+          Add to Cart
+        </Button>
       </CardActions>
     </Card>
   )
